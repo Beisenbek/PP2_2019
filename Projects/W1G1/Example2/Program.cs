48689b995f3859e4f8db7dfb6ac352f22f5c67d5
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2019-01-25 11:46:52 +0600

    'init'

---
+++ Projects/W1G1/Example2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            int aa = int.Parse(a);
+            int bb = int.Parse(b);
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(aa + bb);
+        }
+    }
+}
