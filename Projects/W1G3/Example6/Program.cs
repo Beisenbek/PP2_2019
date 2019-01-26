2c53c360e8691f241fd4ba480236f25b10c88e07
Бейсенбек Байсаков <bsnbk@outlook.com>
2019-01-26 12:29:54 +0600

    upd

---
+++ Projects/W1G3/Example6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line1 = Console.ReadLine();
+            string line2 = Console.ReadLine();
+
+            int n = int.Parse(line1);
+            //string[] numsStr = line2.Split(new char[] { ',', ' ', ';', '#', '$' });
+            string[] numsStr = line2.Split();
+
+            for (int i = 0; i < numsStr.Length; ++i)
+            {
+                int x = int.Parse(numsStr[i]);
+                for (int j = 0; j < 2; ++j)
+                {
+                    Console.Write(x + " ");
+                }
+
+            }
+        }
+    }
+}
