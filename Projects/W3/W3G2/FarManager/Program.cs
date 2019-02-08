977ec551db82fc7e04eccfee9aab00666f46d808
Бейсенбек Байсаков <bsnbk@outlook.com>
2019-02-08 16:02:17 +0600

    'upd'

---
+++ Projects/W3/W3G2/FarManager/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarManager
+{
+    enum FSIMode
+    {
+        Dir,
+        File,
+        Zip
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FSIMode mode = FSIMode.Zip;
+
+            int mode2 = 2;
+            //0 - dir
+            //1 - file
+            //2 - zip
+            //..
+            if(mode2 == 0)
+            {
+                Console.WriteLine("DIR!");
+            }
+            else if (mode2 == 1)
+            {
+                Console.WriteLine("FILE!");
+            }
+            else if (mode2 == 2)
+            {
+                Console.WriteLine("ZIP!");
+            }
+            //..
+            //...
+            //..
+            if (mode == FSIMode.Dir)
+            {
+                Console.WriteLine("DIR!");
+            }else if(mode == FSIMode.File)
+            {
+                Console.WriteLine("FILE!");
+            }else if(mode == FSIMode.Zip)
+            {
+                Console.WriteLine("ZIP!");
+            }
+        }
+    }
+}
---
+++ Projects/W3/W3G2/FarManager2/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarManager2
+{
+    enum ViewMode
+    {
+        Dir,
+        File
+    }
+
+    class Layer
+    {
+        public FileSystemInfo[] Content
+        {
+            get;
+            set;
+        }
+
+        public int SelectedItem
+        {
+            get;
+            set;
+        }
+
+        public void Draw()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            for (int i = 0; i < Content.Length; ++i)
+            {
+                if (i == SelectedItem)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine(Content[i].Name);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\test");
+            Stack<Layer> history = new Stack<Layer>();
+            history.Push(
+                new Layer
+                {
+                    Content = dir.GetFileSystemInfos()
+                }
+            );
+
+            ViewMode viewMode = ViewMode.Dir;
+            bool esc = false;
+            while (!esc)
+            {
+                if (viewMode == ViewMode.Dir)
+                {
+                    history.Peek().Draw();
+                }
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        history.Peek().SelectedItem--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        history.Peek().SelectedItem++;
+                        break;
+                    case ConsoleKey.Enter:
+                        int x = history.Peek().SelectedItem;
+                        FileSystemInfo fileSystemInfo = history.Peek().Content[x];
+
+                        if (fileSystemInfo.GetType() == typeof(DirectoryInfo))
+                        {
+                            viewMode = ViewMode.Dir;
+                            DirectoryInfo selectedDir = fileSystemInfo as DirectoryInfo;
+                            history.Push(new Layer { Content = selectedDir.GetFileSystemInfos() });
+                        }
+                        else
+                        {
+                            viewMode = ViewMode.File;
+                            using (FileStream fs = new FileStream(fileSystemInfo.FullName, FileMode.Open, FileAccess.Read))
+                            {
+                                using (StreamReader sr = new StreamReader(fs))
+                                {
+                                    Console.BackgroundColor = ConsoleColor.White;
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.WriteLine(sr.ReadToEnd());
+                                }
+                            }
+                        }
+                        break;
+                    case ConsoleKey.Backspace:
+                        if (viewMode == ViewMode.Dir)
+                        {
+                            history.Pop();
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.White;
+                            viewMode = ViewMode.Dir;
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        esc = true;
+                        break;
+                }
+            }
+        }
+    }
+}
