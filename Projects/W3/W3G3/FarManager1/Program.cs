3ab78e0f7d1f038a5793c4e0c7757e1d0f08bbd2
Бейсенбек Байсаков <bsnbk@outlook.com>
2019-02-09 11:59:55 +0600

    'upd'

---
+++ Projects/W3/W3G3/FarManager1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarManager1
+{
+
+    enum ObjectInfo
+    {
+        DIR,
+        FILE,
+        ZIP
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isDirInfo = false;
+            ///...
+            ///
+
+            if (isDirInfo == true)
+            {
+                Console.WriteLine("DIR!");
+            }
+            else
+            {
+                Console.WriteLine("FILE!");
+            }
+
+            int isDirInfo2 = 2;
+            /// 0 - DIR
+            /// 1 - FILE
+            /// 2 - ZIP
+
+            if (isDirInfo2 == 0)
+            {
+                Console.WriteLine("DIR!");
+            }
+            else if (isDirInfo2 == 1)
+            {
+                Console.WriteLine("FILE!");
+            }
+            else if (isDirInfo2 == 2)
+            {
+                Console.WriteLine("ZIP!");
+            }
+
+
+            ObjectInfo objectInfo = default(ObjectInfo);
+
+            if (objectInfo == ObjectInfo.DIR)
+            {
+                Console.WriteLine("DIR!");
+            }
+            else if (objectInfo == ObjectInfo.FILE)
+            {
+                Console.WriteLine("FILE!");
+            }
+            else if (objectInfo == ObjectInfo.ZIP)
+            {
+                Console.WriteLine("ZIP!");
+            }
+
+
+            switch (objectInfo)
+            {
+                case ObjectInfo.DIR:
+                    Console.WriteLine("DIR!");
+                    break;
+                case ObjectInfo.FILE:
+                    Console.WriteLine("FILE!");
+                    break;
+                case ObjectInfo.ZIP:
+                    Console.WriteLine("ZIP!");
+                    break;
+                default:
+                    Console.WriteLine("DEFAULT!");
+                    break;
+            }
+
+
+
+        }
+    }
+}
---
+++ Projects/W3/W3G3/FarManager2/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarManager2
+{
+    class Layer
+    {
+        public FileSystemInfo[] Content
+        {
+            get;
+            set;
+        }
+
+        int selectedItem;
+        /*
+
+        public int GetSelectedItem()
+        {
+            return selectedItem;
+        }
+        public void SetSelectedItem(int value)
+        {
+            selectedItem = value;
+        }*/
+
+
+
+        public int SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    selectedItem = Content.Length - 1;
+                }
+                else if (value >= Content.Length)
+                {
+                    selectedItem = 0;
+                }
+                else
+                {
+                    selectedItem = value;
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            for (int i = 0; i < Content.Length; ++i)
+            {
+                if(i == SelectedItem)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine(Content[i].Name);
+            }
+        }
+    }
+
+    enum FarMode
+    {
+        FileView,
+        DirectoryView
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo root = new DirectoryInfo(@"C:\test2");
+            Stack<Layer> history = new Stack<Layer>();
+            FarMode farMode = FarMode.DirectoryView;
+
+            history.Push(
+                new Layer
+                {
+                    Content = root.GetFileSystemInfos(),
+                    SelectedItem = 0
+                });
+
+            while (true)
+            {
+                if (farMode == FarMode.DirectoryView)
+                {
+                    history.Peek().Draw();
+                }
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        history.Peek().SelectedItem--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        history.Peek().SelectedItem++;
+                        break;
+                    case ConsoleKey.Enter:
+                        int x = history.Peek().SelectedItem;
+                        FileSystemInfo fileSystemInfo = history.Peek().Content[x];
+                        if (fileSystemInfo.GetType() == typeof(DirectoryInfo))
+                        {
+                            DirectoryInfo d = fileSystemInfo as DirectoryInfo;
+                            history.Push(new Layer { Content = d.GetFileSystemInfos(), SelectedItem = 0 });
+                        }
+                        else
+                        {
+                            farMode = FarMode.FileView;
+                            using (FileStream fs = new FileStream(fileSystemInfo.FullName, FileMode.Open, FileAccess.Read))
+                            {
+                                using (StreamReader sr = new StreamReader(fs))
+                                {
+                                    Console.BackgroundColor = ConsoleColor.White;
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.Clear();
+                                    Console.WriteLine(sr.ReadToEnd());
+                                }
+                            }
+                        }
+                        break;
+                    case ConsoleKey.Backspace:
+                        if (farMode == FarMode.DirectoryView)
+                        {
+                            history.Pop();
+                        }
+                        else if(farMode == FarMode.FileView)
+                        {
+                            farMode = FarMode.DirectoryView;
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+                    case ConsoleKey.Delete:
+                        int x2 = history.Peek().SelectedItem;
+                        FileSystemInfo fileSystemInfo2 = history.Peek().Content[x2];
+                        if (fileSystemInfo2.GetType() == typeof(DirectoryInfo))
+                        {
+                            DirectoryInfo d = fileSystemInfo2 as DirectoryInfo;
+                            Directory.Delete(fileSystemInfo2.FullName, true);
+                            history.Peek().Content = d.Parent.GetFileSystemInfos();
+                        }
+                        else
+                        {
+                            FileInfo f = fileSystemInfo2 as FileInfo;
+                            File.Delete(fileSystemInfo2.FullName);
+                            history.Peek().Content = f.Directory.GetFileSystemInfos();
+                        }
+                        history.Peek().SelectedItem--;
+                        break;
+                }
+            }
+        }
+    }
+}
