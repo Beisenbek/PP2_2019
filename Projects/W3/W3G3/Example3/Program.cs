91787f0d51893ffbf2bf1079213ae080d14b0648
Бейсенбек Байсаков <bsnbk@outlook.com>
2019-02-09 13:12:01 +0600

    upd

---
+++ Projects/W3/W3G3/Example3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StreamWriter sw = File.CreateText("hello.txt");
+            sw.WriteLine("test");
+            sw.Close();
+            File.Delete("hello.txt");
+            /*
+
+            FileStream fs =  File.Create("test.txt");
+            fs.Close();
+            File.Delete("test.txt");*/
+        }
+    }
+}
