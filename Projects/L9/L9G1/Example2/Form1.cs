66215b5164217486b508bffef82edf453dc55cb2
Beisenbek Baisakov <beysenbek@gmail.com>
2019-04-19 12:10:23 +0600

    'upd'

--- Projects/L9/L9G1/Example2/Form1.cs
+++ Projects/L9/L9G1/Example2/Form1.cs
@@ -9,39 +9,45 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Example1
+namespace Example2
 {
     public partial class Form1 : Form
     {
-        GraphicsPath gp = new GraphicsPath();
         SolidBrush brush = new SolidBrush(Color.Red);
         int r = 30;
         int a = 130;
         int b = 130;
+        Point cPoint;
 
         public Form1()
         {
             InitializeComponent();
-            gp.AddEllipse(new Rectangle(a - r, b - r, 2 * r, 2 * r));
+           
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.FillPath(brush, gp);
+            e.Graphics.FillEllipse(brush, new Rectangle(cPoint.X - r, cPoint.Y - r, 2 * r, 2 * r));
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            if(gp.IsVisible(e.Location))
-            //if ((e.X - a) * (e.X - a) + (e.Y - b) * (e.Y - b) <= r * r)
+            if (e.Button == MouseButtons.Left)
             {
                 brush.Color = Color.Green;
+                cPoint = e.Location;
             }
             else
             {
                 brush.Color = Color.Red;
             }
             Refresh();
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            cPoint = e.Location;
         }
     }
 }
